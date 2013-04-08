5db5860295805aa9559a7feb9d3ef94fef0052a3
faheem <faheem@faheempatel.com>
2013-04-08 06:37:23 +0100

    Display more info
    Decided not to integrate maps (unless I can find a nicer looking way to
    do it)

--- countdown.rb
+++ countdown.rb
@@ -3,22 +3,36 @@ require 'haml'
 require 'date'
 
 get '/' do
-    @exams = {
-                'ecs401u' => ["Procedural Programming", "08-05-2013"],
-                'ecs404u' => ["Computer Systems and Networks", "01-05-2013"],
-                'ecs407u' => ["Logic and Discrete Structures", "22-05-2013"],
-                'ecs414u' => ["Object-Oriented Programming", "20-05-2013"],
-                'ecs417u' => ["Fundamentals of Web Technology", "17-05-2013"],
-                'ecs419u' => ["Information Systems Analysis", "13-05-2013"],
-                'ecs420u' => ["Language and Communication", "21-05-2013"]
+    locations = {
+        'York Hall'   => [51.530135, -0.054117],
+        'Town Hall'   => [51.536636, -0.020864],
+        'Great Hall'  => [51.524586, -0.040669],
+        'Sports Hall' => [51.524586, -0.040669],
+        'Banc-113'    => [52.524586, -0.040669]
+    }
+
+    exams = {
+                'ecs401u' => ["Procedural Programming", "08-05-2013", "10:00","Banc-113"],
+                'ecs404u' => ["Computer Systems and Networks", "01-05-2013", "10:00", "Great Hall"],
+                'ecs407u' => ["Logic and Discrete Structures", "22-05-2013", "10:00","Town Hall"],
+                'ecs414u' => ["Object-Oriented Programming", "20-05-2013", "10:00", "TBA"],
+                'ecs417u' => ["Fundamentals of Web Technology", "17-05-2013", "10:00", "Banc-113"],
+                'ecs419u' => ["Information Systems Analysis", "13-05-2013", "10:00", "Sports Hall"],
+                'ecs420u' => ["Language and Communication", "21-05-2013", "14:30", "York Hall"]
              }
 
-    @times = []
-    @exams.each do |module_code, array|
-        time_left = difference(array[1])
-        @times << [array[0], time_left]
+    @to_display = []
+
+    exams.each do |module_code, array|
+        subject     = array[0]
+        days_left   = difference(array[1])
+        start_time  = array[2]
+        venue       = array[3]
+        #coordinates = locations[venue]
+        @to_display << [subject, days_left, start_time, venue]
     end
-    @times = @times.sort { |a, b| a[1] <=> b[1] }
+    # Sort by days left (fewer first)
+    @to_display = @to_display.sort { |a, b| a[1] <=> b[1] }
     erb :index
 end
 
