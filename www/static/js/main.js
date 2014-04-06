46b8b3805dd004d075ebc8f6944777eb5a53bc6c
faheem <faheem@faheempatel.com>
2014-04-06 17:43:22 +0100

    Add location info

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -2,31 +2,41 @@ $(document).ready(function() {
   var json = {
     "ecs524u": {
       "name": "Internet Protocols and Applications",
-      "timestamp": "2014-04-28T10:00:00+00:00"
+      "timestamp": "2014-04-28T10:00:00+00:00",
+      "venue": "John Orwell Sports Centre",
+      "googleMapsUrl": "https://goo.gl/maps/Cw8dM"
     },
     "ecs505u": {
       "name": "Software Engineering",
-      "timestamp": "2014-05-06T14:30:00+00:00"
+      "timestamp": "2014-05-06T14:30:00+00:00",
+      "venue": "Sports Hall"
     },
     "ecs518u": {
       "name": "Operating Systems",
-      "timestamp": "2014-05-08T14:30:00+00:00"
+      "timestamp": "2014-05-08T14:30:00+00:00",
+      "venue": "BANC-113"
     },
     "ecs522u": {
       "name": "Graphical User Interfaces",
-      "timestamp": "2014-05-15T14:30:00+00:00"
+      "timestamp": "2014-05-15T14:30:00+00:00",
+      "venue": "Octagon"
     },
     "ecs519u": {
       "name": "Database Systems",
-      "timestamp": "2014-05-16T10:00:00+00:00"
+      "timestamp": "2014-05-16T10:00:00+00:00",
+      "venue": "Stratford Town Hall",
+      "googleMapsUrl": "https://goo.gl/maps/EcVmB"
     },
     "ecs509u": {
       "name": "Probability and Matrices",
-      "timestamp": "2014-05-19T14:30:00+00:00"
+      "timestamp": "2014-05-19T14:30:00+00:00",
+      "venue": "John Orwell Sports Centre",
+      "googleMapsUrl": "https://goo.gl/maps/Cw8dM"
     },
     "ecs510u": {
       "name": "Algorithms and Data Structures in an Object-Oriented Framework",
-      "timestamp": "2014-05-23T10:00:00+00:00"
+      "timestamp": "2014-05-23T10:00:00+00:00",
+      "venue": "Octagon"
     }
   };
 
@@ -45,14 +55,21 @@ $(document).ready(function() {
     return Math.round(difference/ONE_DAY);
   }
 
-  function appendExam(div, heading, size, timestamp) {
-    var date = new Date(timestamp);
+  function appendExam(div, exam, size) {
+    var date = new Date(exam.timestamp);
+    var location = '<p>' + exam.venue + '</p>';
+    if (exam.googleMapsUrl) {
+      location = '<p>' + exam.venue
+        + '<br/>(<a href="'
+        + exam.googleMapsUrl
+        + '">Google Maps</a>)</p>';
+    }
     div.append(
         '<div class="exam">'
-      + '<h' + size + '>' + heading.toUpperCase() + '</h' + size + '>'
+      + '<h' + size + '>' + exam.name.toUpperCase() + '</h' + size + '>'
       + '<p><strong>in ' + daysBetween(date) + ' Days</strong></p>'
       + '<p>' + date.toUTCString().slice(0, -7) + ' GMT</p>'
-      + '<p>Location: To be announced</p>'
+      + location
       + '</div>'
     );
   }
@@ -60,9 +77,9 @@ $(document).ready(function() {
   for (var module in json) {
     var exam = json[module];
     if (exam.timestamp === '2014-04-28T10:00:00+00:00') {
-      appendExam($('.next-up'), exam.name, 1, exam.timestamp);
+      appendExam($('.next-up'), exam, 1);
     } else {
-      appendExam($('.later'), exam.name, 3, exam.timestamp);
+      appendExam($('.later'), exam, 3);
     }
   }
 });
